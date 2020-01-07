36e73ce7edf71f91b28a82e5000ef45a2b1c4190
でんぱ <d3npa@pm.me>
2020-01-07 14:32:02 +0900

    Push source code

---
+++ simple-lang.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    var head = document.querySelector("head");
+    var languageStyle = document.createElement("style");
+    var language = document.documentElement.getAttribute("lang");
+    languageStyle.setAttribute("id", "language-style");
+    head.appendChild(languageStyle);
+    setLanguage(language);
+}
+
+function setLanguage(language) {
+    var languageStyle = document.querySelector("#language-style");
+    languageStyle.innerHTML = "t { display: none; } t[" + language + "] { display: inline-block !important; }";
+}
